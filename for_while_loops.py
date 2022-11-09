2b3b4c98e1fb225b11637489d6f29e5e6d050c07
Kyanh Pham <kyanh.pham@mths.ca>
2022-11-09 16:24:57 +0000

    forwhileloop cpp unfinished

---
+++ for_while_loops.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Created by: Kyanh Pham
+# Created on: Oct 2022
+# This program uses for while loops
+
+
+def main():
+
+    loop_counter = 0
+
+    # This function finds the square of all positive integers
+    #  up to the given input
+
+    # Input
+    positive_string = input("Enter an integer >= 0: ")
+
+    # Process and output
+    try:
+        positive_float = float(positive_string)
+        positive_integer = int(positive_float)
+        if positive_integer == positive_float:
+            if positive_integer >= 0:
+                for loop_counter in range(positive_integer + 1):
+                    squared_number = loop_counter * loop_counter
+                    print("{0}² = {1}".format(loop_counter, squared_number))
+            else:
+                print("{0} is not positive.".format(positive_integer))
+        else:
+            print("{0} is not an integer".format(positive_float))
+    except ValueError:
+        print("{0} is not a valid input".format(positive_string))
+    finally:
+        print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
